7c8c7a834d9d6dd87c7500c010b83b92be40e153
parvana07 <peroinitworld@gmail.com>
2020-05-15 18:45:56 -0700

    added shop page, collection item and collection-preview components

--- src/App.js
+++ src/App.js
@@ -2,42 +2,17 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
-
-const Hats = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats</h1>
-    </div>
-  );
-};
-// const Homepage = (props) => {
-//   console.log(props);
-//   return (
-//     <div>
-//       <button onClick={() => props.history.push("/topic")}>Topic</button>
-//       <h1>Homepage</h1>
-//     </div>
-//   );
-// };
-
-const TopicDetail = (props) => {
-  // console.log(props.match.params.topicId);
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic Detail Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Homepage} />
-      {/* <Route exact path="/topic" component={Topic} /> */}
-      <Route exact path="/shop/hats" component={Hats} />
-      <Route path="/hhh/topic/:topicId" component={TopicDetail} />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        {/* <Route exact path="/topic" component={Topic} /> */}
+        <Route exact path="/shop" component={ShopPage} />
+        {/* <Route path="/hhh/topic/:topicId" component={TopicDetail} /> */}
+      </Switch>
     </div>
   );
 }
